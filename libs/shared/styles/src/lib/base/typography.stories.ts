17bccbf97b973f348b8bdcaec206c5b5825ce2e9
Arjen <4863062+the-ult@users.noreply.github.com>
2023-10-26 20:25:04 +0200

    chore: add sample code for typography

---
+++ libs/shared/styles/src/lib/base/typography.stories.ts
@@ -0,0 +1,22 @@
+export default {
+  title: 'Styling / Typography',
+};
+
+export const Default = {
+  render: () => ({
+    template: `
+    <body>
+      <h1>Dit is een &lt;h1&gt; tekst</h1>
+      <h2>Dit is een &lt;h2&gt; tekst</h2>
+      <h3>Dit is een &lt;h3&gt; tekst</h3>
+      <h4>Dit is een &lt;h4&gt; tekst</h4>
+      <h5>Dit is een &lt;h5&gt; tekst</h5>
+      <h6>Dit is een &lt;h6&gt; tekst</h6>
+      <br>
+      <p>Dit is een &lt;p&gt; tekst</p>
+      <p><small>Dit is een &lt;p&gt;&lt;small&gt; tekst</small></p>
+    </body>
+    `,
+  }),
+};
+
